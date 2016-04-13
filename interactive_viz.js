a2c1e81c6d23f6755f6208ff098159f55876121c
Steven Finkelstein <sfinkelstein20@gmail.com>
2016-04-13 12:56:49 -0400

    Call in BEA API
    Read in the objects and start organizing into arrays

--- interactive_viz.js
+++ interactive_viz.js
@@ -7,12 +7,73 @@ $.getJSON('/api/events')
     eventData = data;
 })
 */
+getKey();
+var api_url = "http://www.bea.gov/api/data?&UserID="+user_id+"&method=GetData&DataSetName=NIPA&Year=2014&TableID=5&Frequency=Q&ResultFormat=json&jsonp=api_test";
+/*function jsonCallback(){
+		"sites":[
+			{
+				"siteName": "JQUERY4U",
+				"domainName": "http://www.jquery4u.com",
+				"description": "#1 jQuery Blog for your Daily News, Plugins, Tuts/Tips & Code Snippets."
+			},
+			{
+				"siteName": "BLOGOOLA",
+				"domainName": "http://www.blogoola.com",
+				"description": "Expose your blog to millions and increase your audience."
+			},
+			{
+				"siteName": "PHPSCRIPTS4U",
+				"domainName": "http://www.phpscripts4u.com",
+				"description": "The Blog of Enthusiastic PHP Scripters"
+			}
+		]};
+*/
+$(document).ready(function(){
+	var myDataArray = []; 
+    $.ajax({
+        url: api_url,
+        dataType: 'jsonp',
+		jsonpCallback: 'api_test',
+		jsonp: 'jsonp',
+		}).done(function(response, error){
+			console.log(error);
+			var dataArray = response.BEAAPI.Results.Data;
+			for(var i=0; i<dataArray.length; i++){
+				var segment = dataArray[i];
+				
+				$('body').append('<p>'+segment.DataValue+ '</p>')
+			}
+		});
+		function api_test () {
+};
+	
+	
+	/*$.ajax({
+		url: api_url,
+		async: false,
+		cache: false,
+		/*jsonpCallback: 'jsonCallback',
+		contentType: "application/json",
+		crossdomain: true,
+		dataType: "jsonp"
+	})
+	.done(function(pizza, error){
+		console.log(error);
+		alert("success");
+		return pizza;
+	});*/
+});
 
+
+
+/*
 $(document).ready(function(){
 	$.getJSON("http://www.bea.gov/api/data?&UserID=E52D1C13-32F0-4FB8-B856-08A7239E7C98&method=GetData&DataSetName=NIPA&Year=2014&TableID=5&Frequency=Q&ResultFormat=json").done
 		
 		console.log(api_data);
 });
+*/
+
 /*
 var api_data = "http://www.bea.gov/api/data?&UserID=E52D1C13-32F0-4FB8-B856-08A7239E7C98&method=GetData&DataSetName=NIPA&Year=2014&TableID=5&Frequency=Q&ResultFormat=json";
 var json = JSON.parse(api_data);
